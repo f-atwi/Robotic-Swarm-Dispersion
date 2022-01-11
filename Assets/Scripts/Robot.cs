129dd3f871cb5e3b70309c34881263821f758395
Fouad Atwi <atwe98@gmail.com>
2022-01-11 20:08:48 +0100

    Edited Scene

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -13,22 +13,50 @@ public class Robot : MonoBehaviour
     [SerializeField]
     private LayerMask layerMask;
 
+    public int sense = 1;
     public float Linear_velocity { get => linear_velocity; set => linear_velocity = value; }
     public float Angular_velocity { get => angular_velocity; set => angular_velocity = value; }
 
-    public bool wallDetection()
+    public int? wallDetection()
     {
+        int? detect = null; ;
+
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3f , layerMask))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            return true;
+            detect = 0;
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 3, Color.white);
-            return false;
         }
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward+Vector3.right).normalized, out hit, 3f, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward + Vector3.right).normalized * hit.distance, Color.yellow);
+            detect = 1;
+            sense = -1;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward + Vector3.right).normalized * 3, Color.white);
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward - Vector3.right).normalized, out hit, 3f, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward - Vector3.right).normalized * hit.distance, Color.yellow);
+            detect = -1;
+            sense = 1;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward - Vector3.right).normalized * 3, Color.white);
+        }
+        return detect;
+
+
+
+
 
     }
 
